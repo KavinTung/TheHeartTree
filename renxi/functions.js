4884481ca96b564aecb9f33530ef544c3c971476
KavinTung <54610015+KavinTung@users.noreply.github.com>
2026-06-24 21:57:19 +0700

    Refactor timeElapse function for better accuracy
    Refactor timeElapse function to include years calculation and improve date handling.

--- renxi/functions.js
+++ renxi/functions.js
@@ -33,24 +33,53 @@ $(window).resize(function() {
 	};
 })(jQuery);
 
-function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "<span class=\"digit\">" + days + "</span> ngày <span class=\"digit\">" + hours + "</span> giờ <span class=\"digit\">" + minutes + "</span> phút <span class=\"digit\">" + seconds + "</span> giây"; 
-	$("#clock").html(result);
+function timeElapse(date) {
+    var current = new Date();
+    var seconds = Math.floor((current.getTime() - date.getTime()) / 1000);
+
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds = seconds % (3600 * 24);
+
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    seconds = seconds % 3600;
+
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    seconds = seconds % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    // Tính số năm tròn kể từ ngày bắt đầu yêu nhau
+    var years = current.getFullYear() - date.getFullYear();
+
+    // Nếu chưa đến ngày kỷ niệm trong năm hiện tại thì trừ đi 1 năm
+    var anniversaryThisYear = new Date(
+        current.getFullYear(),
+        date.getMonth(),
+        date.getDate(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds()
+    );
+
+    if (current < anniversaryThisYear) {
+        years--;
+    }
+
+    var result =
+        "<span class=\"digit\">" + years + "+</span> Năm " +
+        "<span class=\"digit\">(" + days + "</span> ngày " +
+        "<span class=\"digit\">" + hours + "</span> giờ " +
+        "<span class=\"digit\">" + minutes + "</span> phút " +
+        "<span class=\"digit\">" + seconds + ")</span> giây";
+
+    $("#clock").html(result);
 }
